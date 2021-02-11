33a3dc9af6bca2e04bff5adf8276981590367bf1
Florian Savouré <25037788+p0slx@users.noreply.github.com>
2021-02-11 17:38:54 +0100

    Adding JSONFromURL

--- app/src/main/java/com/example/time2watch/MainActivity.java
+++ app/src/main/java/com/example/time2watch/MainActivity.java
@@ -1,24 +1,50 @@
 package com.example.time2watch;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.time2watch.api.Movie;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+
+    public String apiKey = "ccbc42c4b357545c785bb0d1caba6301";
+
+    public JSONObject JSONFromURL(String urlPage) {
+        URL url = null;
+        Scanner sc = null;
+        JSONObject jsonObject = null;
+        try {
+            url = new URL(urlPage);
+            sc = new Scanner(url.openStream());
+            StringBuilder sb = new StringBuilder();
+            while (sc.hasNext()) {
+                sb.append(sc.next());
+            }
+            String result = sb.toString();
+            jsonObject = new JSONObject(result);
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonObject;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        TmdbMovies movies = new TmdbApi("ccbc42c4b357545c785bb0d1caba6301").getMovies();
-//        MovieDb movie = movies.getMovie(5353, "fr");
-//        System.out.println(movie.getHomepage());
-        AsyncTask tmdbMovies = new getTrendingMoviesWeek().execute();
-
+        // https://api.themoviedb.org/3/trending/movie/week?api_key=ccbc42c4b357545c785bb0d1caba6301&language=fr
     }
 
     private class getTrendingMoviesWeek extends AsyncTask<Void, Void, ArrayList<Movie>> {
@@ -30,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected ArrayList<Movie> doInBackground(Void... voids) {
-            System.out.println("teeeeeeeeeeeeeeeeeeeeeeeeeeeeeeest");
+
             return new ArrayList<Movie>();
         }
 
