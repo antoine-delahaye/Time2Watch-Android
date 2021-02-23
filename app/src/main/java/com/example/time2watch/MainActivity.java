cd58b2e6b480a43cc206305aef8793cf3236e22f
Florian Savouré <25037788+p0slx@users.noreply.github.com>
2021-02-23 21:48:38 +0100

    Add GetTrendingMoviesDay

--- app/src/main/java/com/example/time2watch/MainActivity.java
+++ app/src/main/java/com/example/time2watch/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
+import com.example.time2watch.api.GetTrendingMoviesDay;
 import com.example.time2watch.api.GetTrendingMoviesWeek;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // https://api.themoviedb.org/3/trending/movie/week?api_key=ccbc42c4b357545c785bb0d1caba6301&language=fr
-        AsyncTask jsonObject = new GetTrendingMoviesWeek().execute();
+        AsyncTask trendingMoviesWeek = new GetTrendingMoviesWeek().execute();
+        AsyncTask trendingMoviesDay = new GetTrendingMoviesDay().execute();
     }
 }
