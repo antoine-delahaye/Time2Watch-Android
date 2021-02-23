caf69912961c857a0d7dce99a253204e148db8dc
Florian Savouré <25037788+p0slx@users.noreply.github.com>
2021-02-23 21:31:01 +0100

    Move getTrendingMoviesWeek() to a dedicated class

--- app/src/main/java/com/example/time2watch/MainActivity.java
+++ app/src/main/java/com/example/time2watch/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.example.time2watch.classes.Movie;
+import com.example.time2watch.api.GetTrendingMoviesWeek;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -17,8 +18,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
-
-    public String apiKey = "ccbc42c4b357545c785bb0d1caba6301";
 
     public static JsonObject getJSON(String urlPage) {
         JsonObject jsonObject = null;
@@ -43,33 +42,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // https://api.themoviedb.org/3/trending/movie/week?api_key=ccbc42c4b357545c785bb0d1caba6301&language=fr
-        AsyncTask jsonObject = new getTrendingMoviesWeek().execute();
-    }
-
-    private class getTrendingMoviesWeek extends AsyncTask<Void, Void, Movie[]> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected Movie[] doInBackground(Void... voids) {
-            JsonObject jsonObject = getJSON("https://api.themoviedb.org/3/trending/movie/week?api_key=" + apiKey + "&language=fr");
-            Gson gson = new Gson();
-            JsonElement jsonElement = jsonObject.get("results");
-            Movie[] moviesArray = gson.fromJson(jsonElement, Movie[].class);
-            for (Movie movie : moviesArray) {
-                movie.setBackdrop_path("https://www.themoviedb.org/t/p/original" + movie.getBackdrop_path());
-                movie.setPoster_path("https://www.themoviedb.org/t/p/original" + movie.getPoster_path());
-            }
-            return moviesArray;
-        }
-
-        @Override
-        protected void onPostExecute(Movie[] movies) {
-            super.onPostExecute(movies);
-            Log.d("getTrendingMoviesWeek", Arrays.toString(movies));
-            Log.d("getTrendingMoviesWeek", String.valueOf(movies.length) + " Movies");
-        }
+        AsyncTask jsonObject = new GetTrendingMoviesWeek().execute();
     }
 }
---
+++ app/src/main/java/com/example/time2watch/api/GetTrendingMoviesWeek.java
@@ -0,0 +1,44 @@
+package com.example.time2watch.api;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.example.time2watch.classes.Movie;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.util.Arrays;
+
+import static com.example.time2watch.MainActivity.getJSON;
+
+
+public class GetTrendingMoviesWeek extends AsyncTask<Void, Void, Movie[]> {
+
+    public String apiKey = "ccbc42c4b357545c785bb0d1caba6301";
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Movie[] doInBackground(Void... voids) {
+        JsonObject jsonObject = getJSON("https://api.themoviedb.org/3/trending/movie/week?api_key=" + apiKey + "&language=fr");
+        Gson gson = new Gson();
+        JsonElement jsonElement = jsonObject.get("results");
+        Movie[] moviesArray = gson.fromJson(jsonElement, Movie[].class);
+        for (Movie movie : moviesArray) {
+            movie.setBackdrop_path("https://www.themoviedb.org/t/p/original" + movie.getBackdrop_path());
+            movie.setPoster_path("https://www.themoviedb.org/t/p/original" + movie.getPoster_path());
+        }
+        return moviesArray;
+    }
+
+    @Override
+    protected void onPostExecute(Movie[] movies) {
+        super.onPostExecute(movies);
+        Log.d("getTrendingMoviesWeek", Arrays.toString(movies));
+        Log.d("getTrendingMoviesWeek", String.valueOf(movies.length) + " Movies");
+    }
+}
