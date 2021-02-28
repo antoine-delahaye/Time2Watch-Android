4d84f1d4777adc5c9876cf3b92e4f9bbe0bcc169
Florian Savouré <25037788+p0slx@users.noreply.github.com>
2021-02-28 23:06:08 +0100

    Revove useless filter and add one remaining in DiscoverTVShows

--- app/src/main/java/com/example/time2watch/api/DiscoverTVShowsAPI.java
+++ app/src/main/java/com/example/time2watch/api/DiscoverTVShowsAPI.java
@@ -16,18 +16,10 @@ import static com.example.time2watch.utils.Utils.getJSON;
 public class DiscoverTVShowsAPI extends AsyncTask<String, Void, TVShow[]> {
     public static final String POPULARITY_ASC = "popularity.asc";
     public static final String POPULARITY_DESC = "popularity.desc";
-    public static final String RELEASE_DATE_ASC = "release_date.asc";
-    public static final String RELEASE_DATE_DESC = "release_date.desc";
-    public static final String REVENUE_ASC = "revenue.asc";
-    public static final String REVENUE_DESC = "revenue.desc";
-    public static final String PRIMARY_RELEASE_DATE_ASC = "primary_release_date.asc";
-    public static final String PRIMARY_RELEASE_DATE_DESC = "primary_release_date.desc";
-    public static final String ORIGINAL_TITLE_ASC = "original_title.asc";
-    public static final String ORIGINAL_TITLE_DESC = "original_title.desc";
+    public static final String FIRST_AIR_DATE_ASC = "first_air_date.asc";
+    public static final String FIRST_AIR_DATE_DESC= "first_air_date.desc";
     public static final String VOTE_AVERAGE_ASC = "vote_average.asc";
     public static final String VOTE_AVERAGE_DESC = "vote_average.desc";
-    public static final String VOTE_COUNT_ASC = "vote_count.asc";
-    public static final String VOTE_COUNT_DESC = "vote_count.desc";
 
     @Override
     protected void onPreExecute() {
